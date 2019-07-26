8acc0983f986c81feb53c40abd49ba1e696fee55
Rohit Singh <40518677+sourcecode369@users.noreply.github.com>
2019-07-26 17:45:54 +0530

    Add all files

---
+++ Portal/src/com/portal/dao/EmployeeDao.java
@@ -0,0 +1,8 @@
+package com.portal.dao;
+
+public interface EmployeeDao {
+	
+	public boolean validate(int empid,String password);
+
+
+}
---
+++ Portal/src/com/portal/utility/ConnectionProvider.java
@@ -0,0 +1,15 @@
+package com.portal.utility;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class ConnectionProvider {
+	
+	public static Connection getDBCOnnection() throws Exception
+	{
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+		Connection conn=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","hr","hr");
+		return conn;
+	}
+
+}
